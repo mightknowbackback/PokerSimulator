b261576e5bafb7c1bfdd848af84f5dec506daf2b
mightknow <michaeljohnbackes@gmail.com>
2020-10-27 16:40:17 -0500

    Add Action enum.

---
+++ PokerSimulator/Model/Action.swift
@@ -0,0 +1,15 @@
+//
+//  Action.swift
+//  PokerSimulator
+//
+//  Created by mightknow on 10/27/20.
+//
+
+import Foundation
+
+enum Action : String, CaseIterable {
+    case check
+    case fold
+    case call
+    case raise
+}
